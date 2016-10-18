223cc93566624d6f1627bd050fac09b05cad95eb
kgbercan <kgbercan@gmail.com>
2016-10-17 21:56:33 -0400

    working on func to line up markup with words
    doesn’t work for tone tier…

--- textgridToCsv.py
+++ textgridToCsv.py
@@ -1,89 +1,114 @@
 import re
+import csv
 
-textgrid1 = open("karina_f2bcprlp1.TextGrid")
-print(".TextGrid to be read: " + textgrid1.name)
 
-#regexes
-itemTier = re.compile("item \[\d\]:")
-tierClass = re.compile("class = \"(\w+)Tier\"")
-tierName = re.compile("name = \"(\w+)\"")
-boundary = re.compile("(intervals|points) \[\d\]:")
-points = re.compile("(xm(in|ax)|number) = (\d+(\.\d+)?)")
-content = re.compile("(text|mark) = \"(.*)\"")
+def lineUpTiersWithWords(tier, xmax):
+	newList = []
+	i = 0
+	for markup in tier:
+		if((float(markup[0])-.05)<float(xmax[len(newList)])):
+			newList.append(markup[1])
+		else:
+			newList.append("")
+	return(newList)
 
-currentTierType = ""
-currentTierName = ""
+def main():
+	textgrid1 = open("karina_f2bcprlp1.TextGrid")
+	print(".TextGrid to be read: " + textgrid1.name)
+	labelsCSV =  open("melnicoveLabels.csv","a")
+	print(".csv opened for: " + labelsCSV.mode)
 
-#put textgrid in list
-lines = textgrid1.readlines()
+	#regexes
+	itemTier = re.compile("item \[\d+\]:")
+	tierClass = re.compile("class = \"(\w+)Tier\"")
+	tierName = re.compile("name = \"(\w+)\"")
+	boundary = re.compile("(intervals|points) \[\d+\]:")
+	points = re.compile("(xm(in|ax)|number) = (\d+(\.\d+)?)")
+	content = re.compile("(text|mark) = \"(.*)\"")
 
-#line by line
-for i in range(len(lines)):
-	current = lines[i]
+	currentTierType = ""
+	currentTierName = ""
 
-	#if the line declares a new tier
-	if(itemTier.search(current)):
-		#move to next line
-		i+=1
-		#the class line
+	xmin = []
+	xmax = []
+	words = []
+	tones = []
+	breaks = []
+	misc = []
+
+	#put textgrid in list
+	lines = textgrid1.readlines()
+
+	#line by line
+	for i in range(len(lines)):
 		current = lines[i]
-		#set current tier type
-		currentTierType = tierClass.search(current).group(1).lower()
-		#move to next line
-		i+=1
-		#the name line
-		current = lines[i]
-		#set the current tier name
-		currentTierName = tierName.search(current).group(1).lower()
 
-	#if intervals or points
-	if(boundary.search(current)):
-		#move to next line
-		i=i+1
-		current = lines[i]
-		#get min/single point
-		print(points.search(current).group(1) + ": " + points.search(current).group(3))
+		#if the line declares a new tier
+		if(itemTier.search(current)):
+			#move to next line, the class line
+			i+=1
+			current = lines[i]
+			currentTierType = tierClass.search(current).group(1).lower()
+			#move to next line, the name line
+			i+=1
+			current = lines[i]
+			currentTierName = tierName.search(current).group(1).lower()
 
-		#if interval 
-		if(currentTierType=="interval"):
+		#if intervals or points
+		if(boundary.search(current)):
 			#move to next line
 			i=i+1
 			current = lines[i]
-			#get max
-			print(points.search(current).group(1) + ": " + points.search(current).group(3))
-		
-		#move to next line
-		i=i+1
-		current = lines[i]
-		#get text
-		print(content.search(current).group(1) + ": " + content.search(current).group(2))
+			#get min/single point
+			pointType = points.search(current).group(1)
+			point = points.search(current).group(3)
+			print(pointType + ": " + point)
+
+			if(currentTierType=="interval"):
+				#store xmin
+				xmin.append(point)
+
+				#move to next line
+				i=i+1
+				current = lines[i]
+				#get max
+				pointType = points.search(current).group(1)
+				point = points.search(current).group(3)
+				print(pointType + ": " + point)
+
+				#store xmax
+				xmax.append(point)
+			
+			#move to next line
+			i=i+1
+			current = lines[i]
+			#get text
+			contentType = content.search(current).group(1)
+			text = content.search(current).group(2)
+			print(contentType + ": " + text)
+
+			#add word, tone, break or misc to csv
+			if(currentTierName=="words"):
+				words.append(text)
+			elif(currentTierName=="tones"):
+				tones.append((point,text))
+			elif(currentTierName=="breaks"):
+				breaks.append((point,text))
+			elif(currentTierName=="misc"):
+				misc.append((point,text))
+
+	tones = lineUpTiersWithWords(tones,xmax)
+	print(tones)
+	breaks = lineUpTiersWithWords(breaks,xmax)
+	print(breaks)
+	misc = lineUpTiersWithWords(misc,xmax)
 
 
-
-#look for name = "words"
-#for each intervals in words
-	#next row, col "xmin" = xmin
-	#^ row, col "xmax" = xmax
-	#^ row, col "text" = text
-#look for name = "tones"
-#for each points
-	#while number > current row.xmax, increment row
-	#row, col "tones" = mark
-#look for name = "breaks"
-#for each points
-	#while abs(number - current row.xmax) > abs(number - row++.xmax)
-	#increment row
-	#row, col "breaks" = mark
-#look for name = "misc"
-#for each points
-	#while number > current row.xmax, increment row
-	#row, col "misc" = mark
+	textgrid1.close()
+	print(".TextGrid closed: " + str(textgrid1.closed))
+	labelsCSV.close()
+	print(".csv closed: " + str(labelsCSV.closed))
+	print("\n\n")
 
 
-textgrid1.close()
-print(".TextGrid closed: " + str(textgrid1.closed))
-labelsCSV =  open("melnicoveLabels.csv","a")
-print(".csv opened for: " + labelsCSV.mode)
-labelsCSV.close()
-print(".csv closed: " + str(labelsCSV.closed))
-print("\n\n")
+main()
